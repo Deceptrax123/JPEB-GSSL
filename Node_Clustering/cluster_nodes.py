d0de4fd6df747ebaa3cf59ebfb3488579f9821c5
Deceptrax123 <srinitishsri@gmail.com>
2024-12-29 22:03:16 +0530

    Set up and test node clustering on cora, pubmed and amazon

---
+++ Node_Clustering/cluster_nodes.py
@@ -0,0 +1,92 @@
+from torch_geometric.graphgym import init_weights
+from model import ContextEncoder
+from torch_geometric.datasets import Planetoid, Amazon
+from sklearn.manifold import TSNE
+from sklearn.cluster import KMeans
+import torch_geometric.transforms as T
+import torch.multiprocessing as tmp
+from matplotlib.colors import ListedColormap
+from torch import nn
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import os
+import wandb
+import gc
+from dotenv import load_dotenv
+
+
+@torch.no_grad()
+def cluster():
+    z = model(graph.x, graph.edge_index).numpy()
+    projected_2d = tsne_transform.fit_transform(z)
+
+    x_min, x_max = projected_2d[:, 0].min()-1, projected_2d[:, 0].max()+1
+    y_min, y_max = projected_2d[:, 1].min()-1, projected_2d[:, 1].max()+1
+
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.02),
+                         np.arange(y_min, y_max, 0.02))
+    kmeans_transform.fit(projected_2d.astype('double'))
+
+    z_kmeans = kmeans_transform.predict(
+        np.c_[xx.ravel(), yy.ravel()]).reshape(xx.shape)
+
+    cmap_light = ListedColormap(
+        ['#ADD8E6', '#FFB6C1', '#90EE90', '#FFFFE0', '#E6E6FA', '#F08080', '#FFDAB9'])
+    plt.figure(figsize=(8, 6))
+    plt.contourf(xx, yy, z_kmeans, cmap=cmap_light, alpha=0.6)
+
+    plt.scatter(projected_2d[:, 0], projected_2d[:, 1],
+                c=kmeans_transform.labels_, s=50, edgecolor='k', cmap='viridis')
+
+    plt.title('Clustering Boundaries of Node Embeddings')
+    plt.show()
+
+
+if __name__ == '__main__':
+    tmp.set_sharing_strategy('file_system')
+    torch.autograd.set_detect_anomaly(True)
+    load_dotenv('.env')
+
+    inp_name = input("Enter dataset to be used: ")
+    cora_path = os.getenv('Cora')
+    pubmed_path = os.getenv('Pubmed')
+    citeseer_path = os.getenv('CiteSeer')
+    computers_path = os.getenv('Computers')
+    photos_path = os.getenv('Photo')
+
+    if inp_name == 'cora':
+        cmap_light = ListedColormap(
+            ['#ADD8E6', '#FFB6C1', '#90EE90', '#FFFFE0', '#E6E6FA', '#F08080', '#FFDAB9'])
+        dataset = Planetoid(root=cora_path, name='Cora')
+        graph = dataset[0]
+        weights_path = os.getenv("cora_encoder")+"model_140.pt"
+    elif inp_name == 'pubmed':
+        cmap_light = ListedColormap(['#ADD8E6', '#FFB6C1', '#90EE90'])
+        dataset = Planetoid(root=pubmed_path, name='PubMed')
+        graph = dataset[0]
+        weights_path = os.getenv("pubmed_encoder")+"model_130.pt"
+    elif inp_name == 'citeseer':
+        weights_path = os.getenv("citeseer_encoder")+"model_140.pt"
+    elif inp_name == 'computers':
+        cmap_light = ListedColormap(['#ADD8E6', '#FFB6C1', '#90EE90', '#FFFFE0',
+                                    '#E6E6FA', '#F08080', '#FFDAB9', '#D8BFD8', '#E0FFFF', '#FAFAD2'])
+        dataset = Amazon(root=computers_path, name='Computers')
+        graph = dataset[0]
+        weights_path = os.getenv("computer_encoder")+"model_30.pt"
+    elif inp_name == 'photos':
+        cmap_light = ListedColormap(['#ADD8E6', '#FFB6C1', '#90EE90', '#FFFFE0', '#E6E6FA',
+                                     '#F08080', '#FFDAB9', '#D8BFD8'])
+        dataset = Amazon(root=photos_path, name='Photo')
+        graph = dataset[0]
+        weights_path = os.getenv("photo_encoder")+"model_265.pt"
+
+    model = ContextEncoder(in_features=graph.x.size(1))
+    model.load_state_dict(torch.load(
+        weights_path, weights_only=True), strict=True)
+
+    tsne_transform = TSNE(
+        n_components=2, learning_rate='auto', init='random', perplexity=30)
+    kmeans_transform = KMeans(n_clusters=dataset.num_classes)
+
+    cluster()
